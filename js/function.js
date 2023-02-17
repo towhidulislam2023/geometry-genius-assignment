c97c8c694c06a5e984dbf6481f88022fd7e256b5
towhidulislam2023 <towhidulislam2.bd@gmail.com>
2023-02-18 01:27:53 +0600

    twoCardsJsDone

--- js/function.js
+++ js/function.js
@@ -1,1 +1,45 @@
-console.log('Connected Function');
+function getInputFeildValue(inputFeildId) {
+    const inputFeildElement = document.getElementById(inputFeildId);
+    const InputFeildValueAsString = inputFeildElement.value;
+    inputFeildElement.value = ''
+    const inputFeildValue = parseFloat(InputFeildValueAsString);
+     return inputFeildValue;
+    
+ 
+}
+
+function getTextFromTextFeild(textFeildId) {
+    const textFeildElement = document.getElementById(textFeildId);
+    const textFeildvalue = textFeildElement.innerText;
+    return textFeildvalue;
+    
+}
+
+function setValueInTable(count,Pname,area) {
+    const tbody = document.getElementById('tableBody');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${count}</td>
+    <td>${Pname}</td>
+    <td>${area} cm<sup>2</sup></suP></td>
+    <td><button class="btn btn-secondary   bg-blue-500 border-none hover:bg-blue-600">Button</button></td>
+    
+    `
+    tbody.appendChild(tr)
+}
+
+function hideAndDisplay(targetEementid,btnId,) {
+    const element = document.getElementById(targetEementid);
+
+    
+    document.getElementById(btnId).addEventListener('click', function () {
+        if (element.style.display != "block") {
+            
+            element.style.display='block'
+        }
+        else {
+            element.style.display = 'none';
+        }
+
+    })
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,68 @@
 document.getElementById('blogBtn').addEventListener('click', function () {
     window.location.href='../blog.html'
- })
+})
+
+document.getElementById('parallelogrameditSaveValue').addEventListener('click', function () {
+  console.log(document.getElementById('parallelogrameditSaveValue').value); 
+})
+
+
+hideAndDisplay('parallelogrameditBtn','parallelogrameditOnBtn')
+   
+
+let count = 0;
+//triangleBtn
+
+
+document.getElementById('triangleBtn').addEventListener('click', function () {
+    count=count+1
+    const name = getTextFromTextFeild('triangleHeading');
+    const B = getInputFeildValue('inputFeildForB');
+    const H = getInputFeildValue('inputFeildForH');
+    if (isNaN(B)) {
+        alert('input a Number');
+        return;
+        
+    }
+    else if (B < 0) {
+        alert('Positive Number Only')
+        return;
+    }
+    if (isNaN(H)) {
+        alert('input a Number');
+        return;
+    }
+    else if (H < 0) {
+        alert('Positive Number Only')
+        return;
+    }
+    const area = 0.5 * B * H;
+     setValueInTable(count,name,area)
+})
+// Rectangle
+document.getElementById('rectangleBtn').addEventListener('click', function () {
+    count=count+1
+    const name = getTextFromTextFeild('triangleHeading');
+    const width = getInputFeildValue('inputFeildForWidth');
+    const length= getInputFeildValue('inputFeildForLength');
+    if (isNaN(width)) {
+        alert('input a Number');
+        return;
+        
+    }
+    else if (width < 0) {
+        alert('Positive Number Only')
+        return;
+    }
+    if (isNaN(length)) {
+        alert('input a Number');
+        return;
+    }
+    else if (length < 0) {
+        alert('Positive Number Only')
+        return;
+    }
+    const area = width * length;
+     setValueInTable(count,name,area)
+})
+console.log(count);
